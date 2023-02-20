7505ccb1913bef21b1306580b25437d665ae46be
VuKieuAnh <vukieuanh.hnue@gmail.com>
2023-02-20 11:59:20 +0700

    add dispatch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import store from "./store";
 
 function App() {
+  const  dispatch = useDispatch();
   const x = useSelector(state => {
     console.log(state);
-    return state;
+    return state.name;
   });
   return (
-   <></>
+   <>{x}
+     <button onClick={()=>{
+         dispatch({
+             type: 'a',
+             data: {
+                 name: 'KA',
+                 age: 30
+             }
+         })
+     }}>test</button>
+   </>
   );
 }
 
